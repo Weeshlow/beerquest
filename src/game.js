4e8283d353843a6af376cac7fbbb3805d65ec6e1
Brian Wheeler <bwheeler@oculusinfo.com>
2016-07-07 15:29:32 -0400

    Background and stuff

--- src/game.js
+++ src/game.js
@@ -18,8 +18,14 @@ define([
         middleGroup: null,
         backGroup: null,
 
-        beer2: null,
-        beer1: null,
+        beers: null,
+
+        background: null,
+
+        currentDrift: null,
+
+        //beer2: null,
+        //beer1: null,
 
         start: function() {
             this.game = new Phaser.Game(1024, 768, Phaser.AUTO, 'gameWindow', {
@@ -38,47 +44,56 @@ define([
             this.game.load.image('logo', 'assets/phaser.png');
             this.game.load.image('beermug', 'assets/images/beermug.svg'); //Attribution: Twitter
             this.game.load.image('beermug2', 'assets/images/beermug2.svg'); //Attribution: Google (Noto)
+            this.game.load.image('sky', 'assets/images/sky.jpg'); //Attribution: Joniprittie
             this.game.time.advancedTiming = true;
 
         },
 
         create: function() {
 
-            this.game.world.setBounds(-2048, -1536, 2048, 1536);
+            this.game.world.setBounds(-1536, -1152, 1536, 1152);
             this.stableCameraX = this.game.world.centerX - 512;
             this.stableCameraY = this.game.world.centerY - 384;
             this.game.camera.x = this.stableCameraX;
             this.game.camera.y = this.stableCameraY;
 
+            this.background = this.game.add.image(512,384, 'sky');//.anchor.set(0.5);
+            this.background.scale.setTo(2);
+            this.background.anchor.setTo(0.5, 0.5);
+            this.background.fixedToCamera = true;
+
             this.backGroup = this.game.add.group();
             this.middleGroup = this.game.add.group();
             this.frontGroup = this.game.add.group();
 
-            this.beerTwo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'beermug2');
-            this.beerTwo.scale.setTo(0.25, 0.25);
-            this.beerTwo.anchor.setTo(0.5, 0.5);
-            this.beerTwo.inputEnabled = true;
-            this.beerTwo.events.onInputUp.add(this.spriteClick, this);
+            this.beers = [];
 
-            this.beerOne = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'beermug2');
-            this.beerOne.scale.setTo(0.5, 0.5);
-            this.beerOne.anchor.setTo(0.5, 0.5);
+            var beer = null;
+            var distance = null;
+            for (var i = 0; i < 10; i++) {
+                beer = this.game.add.sprite(this.game.rnd.integerInRange(-1024,-512), this.game.rnd.integerInRange(-768,-384), 'beermug2');
+                beer.anchor.setTo(0.5, 0.5);
+                beer.inputEnabled = true;
+                beer.events.onInputUp.add(this.spriteClick, this);
+                distance = this.game.rnd.integerInRange(0,2);
+                if (distance < 1) {
+                    beer.scale.setTo(1);
+                    this.frontGroup.add(beer);
+                } else if (distance < 2) {
+                    beer.scale.setTo(0.5);
+                    this.middleGroup.add(beer);
+                } else {
+                    beer.scale.setTo(0.25);
+                    this.backGroup.add(beer);
+                }
 
-            this.frontGroup.add(this.beerOne);
-            this.middleGroup.add(this.beerTwo);
+                this.beers.push(beer);
 
-
-            //this.beerTo.fixedToCamera = true;
+            }
 
             this.cursors = this.game.input.keyboard.createCursorKeys();
 
-
-
-
-            //this.cameraPos = new Phaser.Point(0, 0)
-            //this.cameraPos.setTo(0, 0);
-
-            //this.httpGet('http://localhost:8080/v2/?key=7d1915a6aa7315b63c14b5464c3e2476/search?q=Goosinator&type=beer', this.addBeerCallback);
+            this.httpGet('http://localhost:8080/v2/?key=7d1915a6aa7315b63c14b5464c3e2476/search?q=Goosinator&type=beer', this.addBeerCallback);
 
         },
 
@@ -92,43 +107,46 @@ define([
             if (this.cursors.left.isDown) {
 
                 this.game.camera.x -= 6;
-                this.middleGroup.x += 2;
+                this.middleGroup.x += 1;
+                this.backGroup.x += 2;
             }
             else if (this.cursors.right.isDown) {
                 this.game.camera.x += 6;
-                this.middleGroup.x -= 2;
+                this.middleGroup.x -= 1;
+                this.backGroup.x -= 2;
             } else {
                 if (this.game.camera.x < this.stableCameraX) {
                     this.game.camera.x += 6;
-                    this.middleGroup.x -= 2;
+                    this.middleGroup.x -= 1;
+                    this.backGroup.x -= 2;
                 }
                 if (this.game.camera.x > this.stableCameraX) {
                     this.game.camera.x -= 6;
-                    this.middleGroup.x += 2;
+                    this.middleGroup.x += 1;
+                    this.backGroup.x += 2;
                 }
             }
-
-
 
             if (this.cursors.up.isDown) {
                 this.game.camera.y -= 6;
                 this.middleGroup.y += 2;
+                this.backGroup.y += 4;
             } else if (this.cursors.down.isDown) {
                 this.game.camera.y += 6;
-                this.middleGroup.y -= 2;
+                this.middleGroup.y -= 1;
+                this.backGroup.y -= 2;
             } else {
                 if (this.game.camera.y < this.stableCameraY) {
                     this.game.camera.y += 6;
-                    this.middleGroup.y -= 2;
+                    this.middleGroup.y -= 1;
+                    this.backGroup.y -= 2;
                 }
                 if (this.game.camera.y > this.stableCameraY) {
                     this.game.camera.y -= 6;
-                    this.middleGroup.y += 2;
+                    this.middleGroup.y += 1;
+                    this.backGroup.y += 2;
                 }
             }
-
-
-
 
 
             // change this value to alter the amount of damping, lower values = smoother camera movement
